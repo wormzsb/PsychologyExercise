759fc6e14154a21d271617c1737cebd9c2625fa7
AxelHu <axelhu@163.com>
2016-03-03 01:24:06 +0800

    add task1 tab page

---
+++ PsyEx/PsyEx/Forms/TaskSettingForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PsyEx.Forms
+{
+    public partial class TaskSettingForm : Form
+    {
+        public TaskSettingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
