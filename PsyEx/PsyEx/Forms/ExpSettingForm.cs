8892494c286bd01115b98b6f712f1f38d6d1f37e
AxelHu <axelhu@163.com>
2016-03-02 21:13:59 +0800

    add ExpSettingForm

---
+++ PsyEx/PsyEx/Forms/ExpSettingForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PsyEx.Forms
+{
+    public partial class ExpSettingForm : Form
+    {
+        public ExpSettingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
