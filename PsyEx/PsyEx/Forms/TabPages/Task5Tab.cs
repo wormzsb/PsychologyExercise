f9dbad0476c8bd608e65226d445dd458683e8d94
zsb zsb <wormzsb@gmail.com>
2016-03-08 00:28:07 +0800

    t5

--- PsyEx/PsyEx/Forms/TabPages/Task5Tab.cs
+++ PsyEx/PsyEx/Forms/TabPages/Task5Tab.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PsyEx.Mapper;
+using PsyEx.Contexts;
 
 namespace PsyEx.Forms.TabPages
 {
     public partial class Task5Tab : UserControl
     {
+        public SettingConfig5 config;
+        public bool defaultFlag = true;
         public Task5Tab()
         {
             InitializeComponent();
