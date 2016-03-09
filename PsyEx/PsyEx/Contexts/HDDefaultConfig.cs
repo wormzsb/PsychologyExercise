77ab938da0e199a79682c396b5478b8725c50046
zsb zsb <wormzsb@gmail.com>
2016-03-09 16:45:46 +0800

    hd默认配置

---
+++ PsyEx/PsyEx/Contexts/HDDefaultConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsyEx.Contexts
+{
+    public class HDDefaultConfig
+    {
+        public static double speed = 200.00;//速度
+        public static double sensibility = 100.00;//灵敏度
+        public static double distance = 10.00;//位移
+        public static double angle = 10.00;//角度
+
+       
+    }
+}
