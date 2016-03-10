39a188293df834aaa845f271d76652972069ff25
zsb zsb <wormzsb@gmail.com>
2016-03-10 21:35:14 +0800

    gdi窗口

---
+++ PsyEx/PsyEx/Dx2D/Task1Play.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PsyEx.Dx2D
+{
+    public partial class Task1Play : Form
+    {
+        public Task1Play()
+        {
+            InitializeComponent();
+        }
+    }
+}
