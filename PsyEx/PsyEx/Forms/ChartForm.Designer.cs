727424df333f8195b09ea6237eeb99959ae4d306
zsb zsb <wormzsb@gmail.com>
2016-03-26 16:27:49 +0800

    图表

--- PsyEx/PsyEx/Forms/ChartForm.Designer.cs
+++ PsyEx/PsyEx/Forms/ChartForm.Designer.cs
@@ -29,25 +29,45 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(5, 5);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(268, 246);
+            this.tabControl1.Size = new System.Drawing.Size(770, 550);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -56,48 +76,125 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(260, 220);
+            this.tabPage1.Size = new System.Drawing.Size(762, 524);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // chart1
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(6, 6);
+            this.chart1.Location = new System.Drawing.Point(5, 5);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(258, 208);
+            this.chart1.Size = new System.Drawing.Size(750, 515);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chart2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(762, 524);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(6, 5);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(750, 515);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.chart3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(762, 524);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Location = new System.Drawing.Point(6, 5);
+            this.chart3.Name = "chart3";
+            this.chart3.Size = new System.Drawing.Size(750, 515);
+            this.chart3.TabIndex = 1;
+            this.chart3.Text = "chart3";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.chart4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(762, 524);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            this.chart4.Location = new System.Drawing.Point(6, 5);
+            this.chart4.Name = "chart4";
+            this.chart4.Size = new System.Drawing.Size(750, 515);
+            this.chart4.TabIndex = 1;
+            this.chart4.Text = "chart4";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.chart5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(762, 524);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // chart5
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea5);
+            this.chart5.Location = new System.Drawing.Point(6, 5);
+            this.chart5.Name = "chart5";
+            this.chart5.Size = new System.Drawing.Size(750, 515);
+            this.chart5.TabIndex = 2;
+            this.chart5.Text = "chart5";
             // 
             // ChartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(775, 557);
             this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "ChartForm";
-            this.Text = "ChartForm";
+            this.Text = "图表";
+            this.Load += new System.EventHandler(this.ChartForm_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -108,5 +205,12 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
     }
 }
--- PsyEx/PsyEx/Forms/ChartForm.cs
+++ PsyEx/PsyEx/Forms/ChartForm.cs
@@ -7,14 +7,143 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PsyEx.Util;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace PsyEx.Forms
 {
     public partial class ChartForm : Form
     {
+        public string task;
+        public List<List<string>> values;
+
         public ChartForm()
         {
             InitializeComponent();
+        }
+
+        private void ChartForm_Load(object sender, EventArgs e)
+        {
+            switch (task)
+            {
+                case "T1":
+                    tabPage1.Text = "位移误差随时间变化的曲线";
+                    chart1.Series.Clear();
+
+                    chart1.Series.Add("distance");
+                    chart1.Series[0].ChartType = SeriesChartType.Line;
+                    chart1.ChartAreas[0].AxisX.Title = "时间(s)";
+                    chart1.ChartAreas[0].AxisY.Title = "位移偏差(pix)";
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    double time = 0;
+                    foreach(List<string> v in values)
+                    {
+                        chart1.Series[0].Points.AddXY(time,DoFormIdentify.toDouble(v[12]));
+                        time += 0.040d;
+                    }
+
+                    tabPage2.Text = "角度误差随时间变化的曲线";
+                    chart2.Series.Clear();
+
+                    chart2.Series.Add("rotate");
+                    chart2.Series[0].ChartType = SeriesChartType.Line;
+                    chart2.ChartAreas[0].AxisX.Title = "时间(s)";
+                    chart2.ChartAreas[0].AxisY.Title = "角度";
+                    chart2.ChartAreas[0].AxisX.Interval = 1;
+
+                    double time1 = 0;
+                    foreach (List<string> v in values)
+                    {
+                        chart2.Series[0].Points.AddXY(time1, DoFormIdentify.toDouble(v[13]));
+                        time1 += 0.040d;
+                    }
+
+                    tabPage3.Text = "目标、瞄准器速度随时间变化的曲线";
+                    chart3.Series.Clear();
+
+                    chart3.Series.Add("目标x轴速度");
+                    chart3.Series.Add("目标y轴速度");
+                    chart3.Series.Add("控制器x轴速度");
+                    chart3.Series.Add("控制器y轴速度");
+                    chart3.Series[0].ChartType = SeriesChartType.Line;
+                    chart3.Series[1].ChartType = SeriesChartType.Line;
+                    chart3.Series[2].ChartType = SeriesChartType.Line;
+                    chart3.Series[3].ChartType = SeriesChartType.Line;
+                    chart3.Legends.Add("目标x轴速度");
+                    chart3.Legends.Add("目标y轴速度");
+                    chart3.Legends.Add("控制器x轴速度");
+                    chart3.Legends.Add("控制器y轴速度");
+                    chart3.Legends[0].DockedToChartArea = "ChartArea1";
+                    chart3.Legends[1].DockedToChartArea = "ChartArea1";
+                    chart3.Legends[2].DockedToChartArea = "ChartArea1";
+                    chart3.Legends[3].DockedToChartArea = "ChartArea1";
+                    chart3.Series[0].Legend = "目标x轴速度";
+                    chart3.Series[1].Legend = "目标y轴速度";
+                    chart3.Series[2].Legend = "控制器x轴速度";
+                    chart3.Series[3].Legend = "控制器y轴速度";
+                    chart3.ChartAreas[0].AxisX.Title = "时间(s)";
+                    chart3.ChartAreas[0].AxisY.Title = "速度(pix/s)";
+                    chart3.ChartAreas[0].AxisX.Interval = 1;
+
+                    double time2 = 0;
+                    foreach (List<string> v in values)
+                    {
+                        chart3.Series[0].Points.AddXY(time2, DoFormIdentify.toDouble(v[15]));
+                        chart3.Series[1].Points.AddXY(time2, DoFormIdentify.toDouble(v[16]));
+                        chart3.Series[2].Points.AddXY(time2, DoFormIdentify.toDouble(v[17]));
+                        chart3.Series[3].Points.AddXY(time2, DoFormIdentify.toDouble(v[18]));
+                        time2 += 0.040d;
+                    }
+
+                    tabPage4.Text = "目标、瞄准器角速度随时间变化的曲线";
+                    chart4.Series.Clear();
+
+                    chart4.Series.Add("目标角速度");
+                    chart4.Series.Add("控制器角速度");
+                    chart4.Series[0].ChartType = SeriesChartType.Line;
+                    chart4.Series[1].ChartType = SeriesChartType.Line;
+                    chart4.Legends.Add("目标角速度");
+                    chart4.Legends.Add("控制器角速度");
+                    chart4.Legends[0].DockedToChartArea = "ChartArea1";
+                    chart4.Legends[1].DockedToChartArea = "ChartArea1";
+                    chart4.Series[0].Legend = "目标角速度";
+                    chart4.Series[1].Legend = "控制器角速度";
+                    chart4.ChartAreas[0].AxisX.Title = "时间(s)";
+                    chart4.ChartAreas[0].AxisY.Title = "角速度";
+                    chart4.ChartAreas[0].AxisX.Interval = 1;
+
+                    double time3 = 0;
+                    foreach (List<string> v in values)
+                    {
+                        chart4.Series[0].Points.AddXY(time2, DoFormIdentify.toDouble(v[0]));
+                        chart4.Series[1].Points.AddXY(time2, DoFormIdentify.toDouble(v[19]));
+                        time3 += 0.040d;
+                    }
+
+                    tabPage5.Text = "轨迹图";
+                    chart5.Series.Clear();
+
+                    chart5.Series.Add("目标运动轨迹");
+                    chart5.Series.Add("追踪轨迹");
+                    chart5.Series[0].ChartType = SeriesChartType.Line;
+                    chart5.Series[1].ChartType = SeriesChartType.Line;
+                    chart5.Legends.Add("目标运动轨迹");
+                    chart5.Legends.Add("追踪轨迹");
+                    chart5.Legends[0].DockedToChartArea = "ChartArea1";
+                    chart5.Legends[1].DockedToChartArea = "ChartArea1";
+                    chart5.Series[0].Legend = "目标运动轨迹";
+                    chart5.Series[1].Legend = "追踪轨迹";
+                    chart5.ChartAreas[0].AxisX.Interval = 50;
+                    chart5.ChartAreas[0].AxisY.Interval = 50;
+
+                    foreach (List<string> v in values)
+                    {
+                        chart5.Series[0].Points.AddXY(DoFormIdentify.toDouble(v[6]), DoFormIdentify.toDouble(v[7]));
+                        chart5.Series[1].Points.AddXY(DoFormIdentify.toDouble(v[8]), DoFormIdentify.toDouble(v[9]));
+                    }
+
+                    break;
+            }
         }
     }
 }
